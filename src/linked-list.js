3d994ef1b2126166bec109d514bd2958f573974b
VladislavBaranov <31881026+VladislavBaranov@users.noreply.github.com>
2017-09-22 13:08:58 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -6,7 +6,27 @@ class LinkedList {
         this.prev = prev;
         this.next = next;}
 
-    append(data) {}
+     append(data) {
+        const node = {
+            data: data,
+            prev: null,
+            next: null
+          }
+          
+          if(this.count === 0) {
+            // If this is the first Node, assign it to head
+            this.head = node;
+          } else {
+            // If not the first node, link it to the last node
+            this.tail.next = node;
+        }
+        
+        this.tail = node;
+        
+        this.count++;
+          
+    }
+
 
     head() {}
 
